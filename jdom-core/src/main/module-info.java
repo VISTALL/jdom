3123cabaf8de9e780d4f09db843509dc7d9e9b41
VISTALL <vistall.valeriy@gmail.com>
2020-01-03 22:36:20 +0300

    more exports

--- jdom-core/src/main/module-info.java
+++ jdom-core/src/main/module-info.java
@@ -5,6 +5,8 @@ open module org.jdom {
     exports org.jdom.adapters;
     exports org.jdom.filter;
     exports org.jdom.input;
+    exports org.jdom.input.sax;
+    exports org.jdom.input.stax;
     exports org.jdom.located;
     exports org.jdom.output;
     exports org.jdom.transform;
